edbc6865e0cbc59ac18e83cd41efe0f8a558dfdf
Yousung Ahn <nug22kr@gmail.com>
2020-10-08 00:21:54 +0900

    make logic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const lottoCount = Array.from(Array(45).keys()).map((num) => num + 1);
-  const lotto = lottoCount
-    .sort((a, b) => Math.random() - Math.random())
-    .slice(0, 6);
+  const [lottos, setLoto] = useState([]);
+
+  const randomLotto = () => {
+    const lottoCount = Array.from(Array(45).keys()).map((num) => num + 1);
+    const lotto = lottoCount
+      .sort((a, b) => Math.random() - Math.random())
+      .slice(0, 6);
+
+    return lotto.sort((a, b) => a - b).join(', ');
+  };
+
+  const makeLotto = () => {
+    const lotto = [randomLotto()];
+
+    setLoto([...lottos, lotto]);
+  };
+
+  useEffect(() => {
+    makeLotto();
+  }, []);
 
   return (
     <div className='App'>
       <header className='App-header'>
+        <button onClick={makeLotto}>생성</button>
         <ul className='un-style'>
-          {lotto
-            .sort((a, b) => a - b)
-            .map((number) => {
-              return <li id={number}>{number}</li>;
-            })}
+          {lottos.map((lotto, idx) => {
+            return <li key={idx}>{lotto}</li>;
+          })}
         </ul>
       </header>
     </div>

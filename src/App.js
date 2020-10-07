7b91b61987983a9f5c36758362631946d7d7f3be
Yousung Ahn <nug22kr@gmail.com>
2020-10-07 23:35:33 +0900

    change readme

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const lottoCount = Array.from(Array(45).keys()).map((num) => num + 1);
+  const lotto = lottoCount
+    .sort((a, b) => Math.random() - Math.random())
+    .slice(0, 6);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {lotto
+            .sort((a, b) => a - b)
+            .map((number) => {
+              return <li id={number}>{number}</li>;
+            })}
+        </ul>
       </header>
     </div>
   );

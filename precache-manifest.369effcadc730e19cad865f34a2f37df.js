55e5300f35dadab753bdc808a394acd11ba05c5a
Yousung Ahn <nug22kr@gmail.com>
2020-10-07 23:41:25 +0900

    Updates

---
+++ precache-manifest.369effcadc730e19cad865f34a2f37df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9451ebb6e75dc76dc5b74957271dde03",
+    "url": "/lotto/index.html"
+  },
+  {
+    "revision": "f79c75e8fa0dfaf70c2b",
+    "url": "/lotto/static/css/main.70b2ed50.chunk.css"
+  },
+  {
+    "revision": "47a0416b29b372420186",
+    "url": "/lotto/static/js/2.3f505bc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lotto/static/js/2.3f505bc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79c75e8fa0dfaf70c2b",
+    "url": "/lotto/static/js/main.3aea292e.chunk.js"
+  },
+  {
+    "revision": "2558faafcdc9cb0b8aac",
+    "url": "/lotto/static/js/runtime-main.481f5f2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lotto/static/media/logo.5d5d9eef.svg"
+  }
+]);

7729ebed4e69f5681a8be5f50c23e73285d871c3
Yousung Ahn <nug22kr@gmail.com>
2020-10-07 23:52:23 +0900

    Updates

--- precache-manifest.8166219305b69bd8e0614003217cc7ca.js
+++ precache-manifest.8166219305b69bd8e0614003217cc7ca.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e1b472b6e4f78a45eed264c9eb044344",
+    "revision": "2a0a3d8304b69274bc4fc655bd94ec01",
     "url": "/lotto/index.html"
   },
   {
